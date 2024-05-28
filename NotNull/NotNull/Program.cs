115c06848d5cc4767b3386532f2156764892d908
AStaroverA <AStaroverA@yandex.ru>
2024-05-28 20:09:54 +0300

    Not Null -in progress

---
+++ NotNull/NotNull/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotNull
+{
+    internal class Program
+    {
+        public class Cake
+        {
+            bool thereIsChoco;
+            bool thereIsCream;
+            int layersQuantity;
+            int candlesQuantity;
+            string wish;
+
+            public Cake(bool thereIsChoco, bool thereIsCream, int layersQuantity, int candlesQuantity, string wish)
+            {
+                this.thereIsChoco = thereIsChoco;
+                this.thereIsCream = thereIsCream;
+                this.layersQuantity = layersQuantity;
+                this.candlesQuantity = candlesQuantity;
+                this.wish = wish;
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Cake
+        }
+    }
+}
